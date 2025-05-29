8d37301f2ebe1368ac3d6becfcd979d59fc4f4c7
arselium <rethorekraken@gmail.com>
2025-05-29 23:02:53 +0300

    L4

--- main.cpp
+++ main.cpp
@@ -1,16 +1,71 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <cstdlib>  // для getenv
 using namespace std;
 
+// Указатель на функцию
+using CommandFunction = void(*)(const vector<string>&);
+
+// Функция split
+vector<string> split(const string& input) {
+    vector<string> tokens;
+    string token;
+
+    for (size_t i = 0; i < input.length(); ++i) {
+        char c = input[i];
+        if (c == ' ') {
+            if (!token.empty()) {
+                tokens.push_back(token);
+                token.clear();
+            }
+            continue;
+        }
+        token += c;
+    }
+
+    if (!token.empty()) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+
+// Функция exit
+void exitShell(const vector<string>& args) {
+    if (args.size() > 1 && args[1] == "0") {
+        exit(0);
+    } else {
+        cout << "command not found\n";
+    }
+}
+
 void runShell() {
-    std::string command;
-    while (true)
-    {
-        std::cout << "$ ";
-        std::getline(std::cin, command);
-        std::cout << command << ": command not found";
-    std::cout << std::endl;
+    string command;
+    
+    map<string, CommandFunction> commandMap;
+    commandMap["exit"] = exitShell;
+
+    while (true) {
+        cout << "$ ";
+        getline(cin, command);
+
+        if (command.empty()) {
+            continue;
+        }
+
+        vector<string> tokens = split(command);
+        string cmdName = tokens[0];
+
+        auto it = commandMap.find(cmdName);
+        if (it != commandMap.end()) {
+            it->second(tokens);
+        } else {
+            std::cout << command << ": command not found";
+            std::cout << std::endl;
+        }
     }
 }
 
