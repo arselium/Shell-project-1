e7042425326923486571b8fffafcd9985f2f1926
arselium <rethorekraken@gmail.com>
2025-05-29 22:27:23 +0300

    L3

--- main.cpp
+++ main.cpp
@@ -3,47 +3,18 @@
 #include <vector>
 using namespace std;
 
-
-// Split(строка, разделитель)
-std::vector<std::string> split(std::string& s, const std::string& delimiter) {
-    std::vector<std::string> tokens;
-    size_t pos = 0;
-    std::string token;
-    while ((pos = s.find(delimiter)) != std::string::npos) {
-        token = s.substr(0, pos);
-        tokens.push_back(token);
-        s.erase(0, pos + delimiter.length());
-    }
-    tokens.push_back(s);
-
-    /* Вывод токенов
-    vector t = split(command, " ");
-        for (int i = 1; i < t.size(); i++){
-            std::cout << t[i] << " ";
-        }*/
-
-    return tokens;
-}
-
-void echo(string command){
-       cout << command.substr(5);
-}
-
-
-
 void runShell() {
     std::string command;
+    while (true)
+    {
         std::cout << "$ ";
         std::getline(std::cin, command);
         std::cout << command << ": command not found";
     std::cout << std::endl;
-
+    }
 }
 
-
 int main() {
-
     runShell();
-
     return 0;
 }
